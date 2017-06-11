89e25b2b47e17c106c33d236efba0a6529a5ab33
yanchang1 <yanchang111@163.com>
2017-06-11 23:35:51 +0800

    complete teacherview

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,12 +1,32 @@
-define(["jquery","template"], function($,template) {
+define(["jquery", "template", "bootstrap"], function($, template) {
     // 加载列表数据
     $.ajax({
         type: "get",
         url: "/api/teacher",
         dataType: "json",
         success: function(data) {
-        	var html=template("teacherInfoTpl",{list: data.result});
-        	$("#teacherInfo").html(html);
+            var html = template("teacherInfoTpl", { list: data.result });
+            $("#teacherInfo").html(html);
+            // 绑定查看讲师信息的单击事件
+            $("#teacherInfo").find(".preview").click(function() {
+                var tcId = $(this).closest("td").attr("data-id");
+                // 请求数据
+                $.ajax({
+                    type: "get",
+                    url: "/api/teacher/view",
+                    data: { tc_id: tcId },
+                    dataType: "json",
+                    success: function(data) {
+                        data.result.tc_hometown = data.result.tc_hometown.split('|').join(' ');
+                        var html = template('teacherModalInfoTpl', data.result);
+                        $('#teacherModalInfo').html(html);
+                        // 显示弹窗
+                        $('#teacherModal').modal();
+
+                    }
+                })
+                return false;
+            });
         }
     })
 })
